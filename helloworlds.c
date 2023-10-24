61329cdee0729ea1e99afde8cd8f6dcccec9cc23
Jory GRZESZCZAK <grz.jory38@gmail.com>
2023-10-24 11:17:43 +0200

    test

--- helloworlds.c
+++ helloworlds.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 
-int main()
-/* My first program in C*/
+void main()
 {
-    printf("Hello Worlds !\n");
+    int NUMBER, SUM, COUNTER;
+
+    SUM = 0;
+    COUNTER = 0;
+
+    while (COUNTER < 4)
+    {
+        printf("Enter un integer :");
+        scanf("%i", &NUMBER);
+        SUM += NUMBER;
+        COUNTER++;
+    }
+
+    printf("The sum is: %i \n", SUM);
+    
 }
--- jory/HelloWorld.c
+++ jory/HelloWorld.c
@@ -1,21 +1,12 @@
 #include <stdio.h>
+#define PI 3.14159
+#define C ':'
 
 void main()
 {
-    int NUMBER, SUM, COUNTER;
-
-    SUM = 0;
-    COUNTER = 0;
-
-    while (COUNTER < 4)
-    {
-        printf("Enter un integer :");
-        scanf("%i", &NUMBER);
-        SUM += NUMBER;
-        COUNTER++;
-    }
-
-    printf("The sum is: %i \n", SUM);
-    
+    int r = 20;
+    float area = PI * r * r;
+    char v = c;
+    printf("The area of the circle %c %d", C, PI);
+    getchar();
 }
-
